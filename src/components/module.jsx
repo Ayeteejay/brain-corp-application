0f842e881c4fb71c9fed5c1506042806030080a9
Adam Jones <adam@ayeteejay.com>
2021-04-28 13:34:38 -0700

    Working on modules

--- src/components/module.jsx
+++ src/components/module.jsx
@@ -8,9 +8,34 @@ box-shadow: 0px 0px 9px -1px rgba(0,0,0,0.3);
 background:${props=>props.theme.secondaryColors.white};
 display:flex;
 transition:500ms;
+flex-flow:column;
 &:hover{
-    -webkit-box-shadow: 5px 5px 9px -1px rgba(0,0,0,0.5); 
-    box-shadow: 5px 5px 9px -1px rgba(0,0,0,0.5);
+    -webkit-box-shadow: 5px 5px 15px 0px rgba(0,0,0,0.5); 
+    box-shadow: 5px 5px 15px 0px rgba(0,0,0,0.5);
+    transform:translate(-10px, -10px)
+}
+img{
+    max-width:100%;
+}
+`
+
+const ContentRow = styled.div`
+display:flex;
+padding:1rem;
+flex-flow:column;
+align-items:center;
+h3{
+    color:${props=>props.theme.primaryColors.blue};
+    font-size:1.25rem;
+    padding:0 0 1rem 0;
+}
+@media (min-width:${props=>props.theme.breakPoints.sm}){
+    justify-content:space-between;
+    flex-flow:row;    
+    padding:1.5rem;
+    h3{
+        padding:0;
+    }
 }
 `
 
@@ -38,10 +63,12 @@ transition:300ms;
 const Module = (props) => {
     return (
         <Card>
-            
+            <img src={props.bannerImg} alt={props.imgAlt}/>
+            <ContentRow>
             <h3>{props.moduleTitle}</h3>
             <InternalLink to={props.pageLink} style={{display: props.internalPage ? "block" : "none"}}>{props.ctaTitle}</InternalLink>
             <ExternalLink href={props.pageLink} style={{display:!props.internalPage ? "block" : "none"}} rel="noreferrer noopener" target="_blank">{props.ctaTitle}</ExternalLink>
+            </ContentRow>
         </Card>
     )
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,13 @@ import Layout from "../components/layout";
 import styled from "styled-components";
 import Module from "../components/module";
 
+import BrainOs from "../images/brainos-logo.svg";
+import Scrubber from "../images/scrubber-front-view.svg";
+import FloorCare from "../images/floor-care.png";
+import Delivery from "../images/delivery.png";
+import FleetManagement from "../images/fleet-management.jpg";
+import ComingSoon from "../images/coming-soon.png";
+
 const OuterContainer = styled.div`
   background-color: ${(props) => props.theme.secondaryColors.lightGrey};
   padding: 4rem;
@@ -11,11 +18,35 @@ const OuterContainer = styled.div`
 `;
 
 const InnerContainer = styled.div`
-  width: 80%;
+  @media (min-width: ${(props) => props.theme.breakPoints.sm}) {
+    width: 80%;
+  }
+  @media (min-width: ${(props) => props.theme.breakPoints.sm}) {
+    h1 {
+      font-size: 3rem;
+    }
+  }
 `;
+const LogoRow = styled.div``;
+const InnerRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BrainOSLogo = styled.img`
+  width: 150px;
+  padding: 0 0 0.5rem 0;
+`;
+
+const ScrubberImg = styled.img`
+  width: 500px;
+`;
+
 const HeroRow = styled.div`
   color: ${(props) => props.theme.secondaryColors.white};
   padding: 5rem;
+  display: flex;
+  justify-content: center;
   background-image: linear-gradient(
     to right,
     ${(props) => props.theme.primaryColors.blue},
@@ -24,38 +55,57 @@ const HeroRow = styled.div`
 `;
 const ModuleRow = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   grid-gap: 2rem;
+  @media (min-width: ${(props) => props.theme.breakPoints.lg}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 const Index = () => {
   return (
     <Layout>
       <HeroRow>
-        <h1>Virtual Sales Demo</h1>
+        <InnerContainer>
+          <InnerRow>
+            <LogoRow>
+              <BrainOSLogo src={BrainOs} alt="BrainOS" />
+              <h1>Virtual Sales Demo</h1>
+            </LogoRow>
+            <ScrubberImg src={Scrubber} alt="Scrubber front view" />
+          </InnerRow>
+        </InnerContainer>
       </HeroRow>
       <OuterContainer>
         <InnerContainer>
           <ModuleRow>
             <Module
+              bannerImg={FloorCare}
+              imgAlt="Image of Scrubber robot operating in store."
               moduleTitle="Floor Care"
               ctaTitle="View module"
               internalPage={true}
               pageLink="/floor-care"
             ></Module>
             <Module
+              bannerImg={Delivery}
+              imgAlt="Image of Tug robot delivery product to store shelf."
               moduleTitle="Delivery"
               ctaTitle="View module"
               internalPage={true}
               pageLink="/delivery"
             ></Module>
             <Module
+              bannerImg={FleetManagement}
+              imgAlt="Image of robots working in store environment."
               moduleTitle="Fleet Management"
               ctaTitle="View module"
               internalPage={false}
               pageLink="https://braincorporation.invisionapp.com/console/share/4R2EQ73TG2"
             ></Module>
             <Module
+              bannerImg={ComingSoon}
+              imgAlt="Image of innovation happening."
               moduleTitle="Coming soon"
               ctaTitle="Learn more"
               internalPage={false}
